36f87ecdf0bc715dd1111463fcacf5f03c85b5ed
Zain Ul Abideen <zaintariq927@gmail.com>
2020-07-01 16:56:47 +0500

    New directory format

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
-const foreCast = require('../public/utils/forecast.js')
+const foreCast = require('../../public/utils/forecast.js')
 
 const port = process.env.PORT || 3000
 const app = express()
